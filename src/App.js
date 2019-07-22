a2ee096f719f6cd0960d1e915ea9b130f05c832d
Aravind Srinivasan <aravindsrinivasan2000@gmail.com>
2019-07-22 19:17:19 +0530

    Code Cleanup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
-import {BrowserRouter,Route,Switch} from 'react-router-dom';
-import Home from './components/home'
-import Navbar from './components/navbar'
-import Team from './components/team/Team'
-import Events from  './components/events/events'
+import {BrowserRouter} from 'react-router-dom';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <div className="App">
-        <Navbar />
-      <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route path="/team" component={Team} />
-      <Route path="/events" component={Events} />
-      </Switch>
-      </div>
+        <h4>Hello World!</h4>
       </BrowserRouter>
     );
   }
