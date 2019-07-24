43407a7f5a34bb028d002d6fede0b26f972fbe38
Vashanth <2000.vashanth@gmail.com>
2019-07-24 21:19:59 +0530

    Events page

--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {Switch, Route } from 'react-router-dom';
 import Landing from '../LandingPage/Landing';
-
+import Events from '../Events/Events'
 
 export default function Routes() {
     return (
         <Switch>
-            <Route path = '/' component = {Landing} />
+            <Route exact path = '/' component = {Landing} />
             <Route path = '/teams' component = {null} />
-            <Route path = '/events' component = {null} />
+            <Route path = '/events' component = {Events} />
             <Route path = '/register/:id' component = {null} />
         </Switch>
     )
