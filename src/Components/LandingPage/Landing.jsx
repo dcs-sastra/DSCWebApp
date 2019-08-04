194d0952d884368f80e2c14f09dc1e8f7a73a639
Aravind Srinivasan <aravindsrinivasan2000@gmail.com>
2019-08-04 16:07:13 +0530

    Added eslint

--- src/Components/LandingPage/Landing.jsx
+++ src/Components/LandingPage/Landing.jsx
@@ -1,135 +1,233 @@
-import React, { Component } from 'react';
-import './Landing.css';
-import abstract1 from '../../assets/abstract1.svg';
-import abstract2 from '../../assets/abstract2.svg';
-import abstract3 from '../../assets/abstract3.svg';
-import abstract4 from '../../assets/abstract4.svg';
-import Typist from 'react-typist';
-import 'react-typist/dist/Typist.css';
-
+import React, { Component } from "react";
+import "./Landing.css";
+import abstract1 from "../../assets/abstract1.svg";
+import abstract2 from "../../assets/abstract2.svg";
+import abstract3 from "../../assets/abstract3.svg";
+import abstract4 from "../../assets/abstract4.svg";
+import Typist from "react-typist";
+import "react-typist/dist/Typist.css";
 
 class Landing extends Component {
-    state = {
-        email:'',
-        phone:'',
-        message:'',
-        typerLine: 0
-      }
-      handleChange = (e) =>
-      {
-        this.setState({
-          [e.target.id] : e.target.value
-        })
-        
-      }
-    
-      handleSubmit = (e) =>{
-        e.preventDefault();
-        console.log(this.state)
-        this.setState({
-          email:'',
-          phone:'',
-          message:''
-        })
-      }
-      incrementLine = () => {
-          this.setState({
-              typerLine: this.state.typerLine + 1
-          });
-      }
-    render() {
-        return (
-            <div className = 'landing_cover'>
-                <div className = 'abstract__wrapper'>
-                    <img src={abstract1} alt="" className = 'landing__abstract'/>
-                    <img src={abstract2} alt="" className = 'landing__abstract'/>
-                    <img src={abstract3} alt="" className = 'landing__abstract'/>
-                    <img src={abstract4} alt="" className = 'landing__abstract'/>
-                </div>
-                <div className = "jumbotron__wrapper text-center">
-                    <div>
-                    <h1>Some attractive caption!</h1>
-                    <h6>By line comes here</h6>
-                    </div>
-                </div>
-                <div className = "terminal">
-                    <div className = 'terminal_menu'>
-                        <span><i className="fas fa-times"></i></span>
-                        <span><i className="far fa-window-maximize"></i></span>
-                        <span><i className="fas fa-window-minimize"></i></span>
-                        <p>Terminal</p>
-                    </div>
-                    <div className = 'terminal_content container'>
-                        <div>
-                        <span className = 'terminal_root_user'>dsc@sastra:</span><span>~/</span>$  <Typist className = 'terminal_typing' onTypingDone = {this.incrementLine} cursor={{ hideWhenDone: true }} avgTypingDelay={50}> &nbsp; We code </Typist>
-                        </div>
-                        {this.state.typerLine >= 1 && <div>
-                        <span className = 'terminal_root_user'>dsc@sastra:</span><span>~/</span>$  <Typist className = 'terminal_typing' onTypingDone = {this.incrementLine} cursor={{ hideWhenDone: true }} avgTypingDelay={50}> &nbsp; We Organise </Typist>
-                        </div>}
-                        {this.state.typerLine >= 2 &&<div>
-                        <span className = 'terminal_root_user'>dsc@sastra:</span><span>~/</span>$  <Typist className = 'terminal_typing' onTypingDone = {this.incrementLine} cursor={{ hideWhenDone: true }} avgTypingDelay={50}> &nbsp; We Learn </Typist>
-                        </div>}
-                        {this.state.typerLine >= 3 &&<div>
-                        <span className = 'terminal_root_user'>dsc@sastra:</span><span>~/</span>$  <Typist className = 'terminal_typing' onTypingDone = {this.incrementLine} cursor={{ hideWhenDone: true }} avgTypingDelay={50}> &nbsp; We Teach </Typist>
-                        </div>}
-                        {this.state.typerLine >= 4 && <div>
-                        <span className = 'terminal_root_user'>dsc@sastra:</span><span>~/</span>$  <Typist className = 'terminal_typing' onTypingDone = {this.incrementLine} avgTypingDelay={50}> &nbsp; We are team DSC! </Typist>
-                        </div>}
-                    </div>
-                </div>
-                <div className = 'about__dsc'>
-                    <div className = "container">
-                        <br/><br/>
-                        <h3 className ='text-center'>About Developer Students Club</h3>
-                        <div className = 'divider'></div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam rerum voluptate minus eaque provident adipisci, ducimus fugiat dignissimos necessitatibus quod a laboriosam saepe, culpa fuga dolorum, sapiente aliquid veniam molestiae.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam minus asperiores incidunt voluptatem, aliquid iusto natus placeat, at maxime eius harum tempore suscipit beatae praesentium saepe est eveniet officiis vel.
-                        </p>
-                    </div>
-                </div>
-                {/* Clusters */}
-                <div className = 'clusters'>
-                    <div className="container">
-                        <h3 className ='text-center'>Clusters</h3>
-                        <div className = 'divider'></div>
-                    </div>
-                </div>
-                {/* Contact Us */}
-                <div className = 'contact_us' id = 'contact'>
-                    <div className = 'container'>
-                    <br/><br/>
-                        <h3 className ='text-center'>Contact Us</h3>
-                        <div className = 'divider'></div>
-                        <div>
-                            <form className="container form_body">
-                                    <div class="form-group">
-                                        <input type="email" class="form-control inp" id="email"  placeholder="Email" value={this.state.email} onChange={this.handleChange}/>
-                                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="text" class="form-control inp" id="phone" placeholder="Phone" value={this.state.phone} onChange={this.handleChange}/>
-                                    </div>
-                                    <div class="form-group shadow-textarea">
-                                    <textarea class="form-control z-depth-1 inp" id="message" rows="3" placeholder="Message" value={this.state.message} onChange={this.handleChange}></textarea>
-                                    </div>
-                                    <button type="button" class="btn btn-success send_btn" onClick={this.handleSubmit}>Send</button>
-                            </form>
-                            <br/>
-                        </div>
+  state = {
+    email: "",
+    phone: "",
+    message: "",
+    typerLine: 0
+  };
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
 
-                    </div>
-                </div>
-                <div className = 'Landing__address container'>
-                    <br/>
-                    <h4>DSC SASTRA - Powered By Google </h4>
-                    Sastra Deemed to be University,<br/>
-                    Thanjavur,<br/>
-                    Tamil Nadu
-                </div>
-                
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+    this.setState({
+      email: "",
+      phone: "",
+      message: ""
+    });
+  };
+  incrementLine = () => {
+    this.setState({
+      typerLine: this.state.typerLine + 1
+    });
+  };
+  render() {
+    return (
+      <div className="landing_cover">
+        <div className="abstract__wrapper">
+          <img src={abstract1} alt="" className="landing__abstract" />
+          <img src={abstract2} alt="" className="landing__abstract" />
+          <img src={abstract3} alt="" className="landing__abstract" />
+          <img src={abstract4} alt="" className="landing__abstract" />
+        </div>
+        <div className="jumbotron__wrapper text-center">
+          <div>
+            <h1>Some attractive caption!</h1>
+            <h6>By line comes here</h6>
+          </div>
+        </div>
+        <div className="terminal">
+          <div className="terminal_menu">
+            <span>
+              <i className="fas fa-times"></i>
+            </span>
+            <span>
+              <i className="far fa-window-maximize"></i>
+            </span>
+            <span>
+              <i className="fas fa-window-minimize"></i>
+            </span>
+            <p>Terminal</p>
+          </div>
+          <div className="terminal_content container">
+            <div>
+              <span className="terminal_root_user">dsc@sastra:</span>
+              <span>~/</span>${" "}
+              <Typist
+                className="terminal_typing"
+                onTypingDone={this.incrementLine}
+                cursor={{ hideWhenDone: true }}
+                avgTypingDelay={50}
+              >
+                {" "}
+                &nbsp; We code{" "}
+              </Typist>
             </div>
-        );
-    }
+            {this.state.typerLine >= 1 && (
+              <div>
+                <span className="terminal_root_user">dsc@sastra:</span>
+                <span>~/</span>${" "}
+                <Typist
+                  className="terminal_typing"
+                  onTypingDone={this.incrementLine}
+                  cursor={{ hideWhenDone: true }}
+                  avgTypingDelay={50}
+                >
+                  {" "}
+                  &nbsp; We Organise{" "}
+                </Typist>
+              </div>
+            )}
+            {this.state.typerLine >= 2 && (
+              <div>
+                <span className="terminal_root_user">dsc@sastra:</span>
+                <span>~/</span>${" "}
+                <Typist
+                  className="terminal_typing"
+                  onTypingDone={this.incrementLine}
+                  cursor={{ hideWhenDone: true }}
+                  avgTypingDelay={50}
+                >
+                  {" "}
+                  &nbsp; We Learn{" "}
+                </Typist>
+              </div>
+            )}
+            {this.state.typerLine >= 3 && (
+              <div>
+                <span className="terminal_root_user">dsc@sastra:</span>
+                <span>~/</span>${" "}
+                <Typist
+                  className="terminal_typing"
+                  onTypingDone={this.incrementLine}
+                  cursor={{ hideWhenDone: true }}
+                  avgTypingDelay={50}
+                >
+                  {" "}
+                  &nbsp; We Teach{" "}
+                </Typist>
+              </div>
+            )}
+            {this.state.typerLine >= 4 && (
+              <div>
+                <span className="terminal_root_user">dsc@sastra:</span>
+                <span>~/</span>${" "}
+                <Typist
+                  className="terminal_typing"
+                  onTypingDone={this.incrementLine}
+                  avgTypingDelay={50}
+                >
+                  {" "}
+                  &nbsp; We are team DSC!{" "}
+                </Typist>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="about__dsc">
+          <div className="container">
+            <br />
+            <br />
+            <h3 className="text-center">About Developer Students Club</h3>
+            <div className="divider"></div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+              rerum voluptate minus eaque provident adipisci, ducimus fugiat
+              dignissimos necessitatibus quod a laboriosam saepe, culpa fuga
+              dolorum, sapiente aliquid veniam molestiae. Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Quisquam minus asperiores
+              incidunt voluptatem, aliquid iusto natus placeat, at maxime eius
+              harum tempore suscipit beatae praesentium saepe est eveniet
+              officiis vel.
+            </p>
+          </div>
+        </div>
+        {/* Clusters */}
+        <div className="clusters">
+          <div className="container">
+            <h3 className="text-center">Clusters</h3>
+            <div className="divider"></div>
+          </div>
+        </div>
+        {/* Contact Us */}
+        <div className="contact_us" id="contact">
+          <div className="container">
+            <br />
+            <br />
+            <h3 className="text-center">Contact Us</h3>
+            <div className="divider"></div>
+            <div>
+              <form className="container form_body">
+                <div class="form-group">
+                  <input
+                    type="email"
+                    class="form-control inp"
+                    id="email"
+                    placeholder="Email"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                  />
+                  <small id="emailHelp" class="form-text text-muted">
+                    We'll never share your email with anyone else.
+                  </small>
+                </div>
+                <div class="form-group">
+                  <input
+                    type="text"
+                    class="form-control inp"
+                    id="phone"
+                    placeholder="Phone"
+                    value={this.state.phone}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div class="form-group shadow-textarea">
+                  <textarea
+                    class="form-control z-depth-1 inp"
+                    id="message"
+                    rows="3"
+                    placeholder="Message"
+                    value={this.state.message}
+                    onChange={this.handleChange}
+                  ></textarea>
+                </div>
+                <button
+                  type="button"
+                  class="btn btn-success send_btn"
+                  onClick={this.handleSubmit}
+                >
+                  Send
+                </button>
+              </form>
+              <br />
+            </div>
+          </div>
+        </div>
+        <div className="Landing__address container">
+          <br />
+          <h4>DSC SASTRA - Powered By Google </h4>
+          Sastra Deemed to be University,
+          <br />
+          Thanjavur,
+          <br />
+          Tamil Nadu
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Landing;
--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -1,139 +1,174 @@
-import React from 'react'
-import './Team.css';
-import teamLogo from '../../assets/team.svg';
-import data from '../../Store.js';
-
+import React from "react";
+import "./Team.css";
+import teamLogo from "../../assets/team.svg";
+import data from "../../Store.js";
 
 export default function Team() {
-    let appDevelopers = [];
-    let iterator = 0;
-    for(let i=1;i<=5;i++) {
-        for(let j=1; j <= i; j++) {
-            let person = data.teams.app[iterator++];
-            if(person) {
-                console.log(person.imgUrl);
-                let url = person.imgUrl;
-                let name = person.name;
-                let jsx = (
-                    <div key = {iterator} className = 'team_member_wrapper'>
-                        <img src= {url} alt="" className = 'team_members' />
-                        <p>{name}</p>
-                    </div>
-                );
-                appDevelopers.push(jsx);
-            }
-        }
-        appDevelopers.push(<><br id = {iterator} /></>)
-    }
-    
-    let nonTech = data.teams.nonTech.map((person,id) => {
-        return (
-            <div className = 'col-4 text-center non_tech' key = {id}>
-                <img src= {person.imgUrl} alt="" className = 'team_members' />
-                <p>{person.name}</p>
-                <p>{person.team}</p>
+  let appDevelopers = [];
+  let iterator = 0;
+  for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= i; j++) {
+      let person = data.teams.app[iterator++];
+      if (person) {
+        console.log(person.imgUrl);
+        let url = person.imgUrl;
+        let name = person.name;
+        let jsx = (
+          <div className="user-card" key={iterator}>
+            <div className="front-side">
+              <img src={url} alt="" />
+              <h6>{name}</h6>
+              <span className="social">
+                <a href="https://linkedin.com" className="fab fa-linkedin"></a>
+                <a className="fab fa-github"></a>
+                <a className="fas fa-basketball-ball"></a>
+              </span>
             </div>
+          </div>
         );
-    })
+        appDevelopers.push(jsx);
+      }
+    }
+    appDevelopers.push(
+      <>
+        <br id={iterator} />
+      </>
+    );
+  }
 
+  let nonTech = data.teams.nonTech.map((person, id) => {
     return (
-        <div className = 'container'>
-            <br/>
-            <h3 className = 'text-center'> Some good caption here </h3>
-            <div className = 'row'>
-                <div className="col-md-6">
-                    <img src={teamLogo} alt="" width = "100%"/>
-                </div>
-                <div className="col-md-6">
-                    <br/><br/>
-                    <div className = 'Lead__div'>
-                        <div className = 'lead__image'>
-                            <img src="https://i.pravatar.cc/200" alt=""/>
-                        </div>
-                        <div>
-                            <h3>Kavin Raju S</h3>
-                            <small className = 'text-secondary'>DSC Lead, App Developer</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <br/>
-            <div>
-                <br/><br/>
-                <div className = 'container'>
-                    <h3 className = 'text-center'> Tech Team </h3>
-                    <div className = 'divider'></div>
-                    <div className = 'tech_team img1'>
-                        <div className = 'row'>
-                            <div style = {{width: '100%'}}>
-                               <div className = 'appDevelopers_wrapper '>
-                                    {appDevelopers}
-                               </div>
-                            
-                            </div>
-                            <div className = 'team_title'>
-                                <h3 className = 'text-left'>App Developers</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <div className = 'tech_team img2'>
-                        <div className = 'row'>
-                        <div style = {{width: '100%'}}>
-                               <div className = 'appDevelopers_wrapper'>
-                                    {appDevelopers}
-                               </div>
-                            
-                            </div>
-                            <div className="team_title">
-                                <h3 className = 'text-left text-white'>Web Developers</h3>
-                            </div>
-                        </div>
-                        
-                    </div>
-                    <div className = 'tech_team img3'>
-                    <div className = 'row'>
-                            <div style = {{width: '100%'}}>
-                               <div className = 'appDevelopers_wrapper'>
-                                    {appDevelopers}
-                               </div>
-                            
-                            </div>
-                            <div className="team_title">
-                                <h3 className = 'text-center'>Machine Learning Enthusiasts</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <div className = 'tech_team img4'>
-                        <div className="row">   
-                        <div style = {{width: '100%'}}>
-                               <div className = 'appDevelopers_wrapper'>
-                                    {appDevelopers}
-                               </div>
-                            
-                            </div>
-                            <div className="team_title">
-                                <h3 className = 'text-left text-white'>Blockchain / AR </h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <br/><br/>
-                <div className = 'container'>
-                    <h3 className = 'text-center'> Non - Tech Team </h3>
-                    <div className = 'divider'></div>
-                    <div className="card nonTechTeam">
-                        <div className="container">
-                            <br/>
-                            <div className = 'row '>
-                                {nonTech}
-                            </div>
-                            <br/>
-                        </div>
-                    </div>
-                </div>
-                <br/><br/>
-            </div>
-            <br/>
+      <div className="col-4 text-center non_tech" key={id}>
+        <img src={person.imgUrl} alt="" className="team_members" />
+        <p>{person.name}</p>
+        <span className="social nontech_social">
+          <a href="https://linkedin.com" className="fab fa-linkedin"></a>
+          <a className="fab fa-github"></a>
+          <a className="fas fa-basketball-ball"></a>
+        </span>
+      </div>
+    );
+  });
+
+  return (
+    <div className="container">
+      <br />
+      <h3 className="text-center"> Some good caption here </h3>
+      <div className="row">
+        <div className="col-md-6">
+          <img src={teamLogo} alt="" width="100%" />
         </div>
-    )
+        <div className="col-md-6">
+          <br />
+          <br />
+          <div className="Lead__div">
+            <div className="lead__image">
+              <img src="https://i.pravatar.cc/200" alt="" />
+            </div>
+            <div>
+              <h3>Kavin Raju S</h3>
+              <small className="text-secondary">DSC Lead, App Developer</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div>
+        <br />
+        <br />
+        <div className="container">
+          <h3 className="text-center"> Tech Team </h3>
+          <div className="divider"></div>
+          <div className="tech_team img1">
+            <div className="row">
+              <div style={{ width: "100%" }}>
+                <div className="appDevelopers_wrapper ">{appDevelopers}</div>
+              </div>
+              <div className="team_title">
+                <h3 className="text-left">Android Developers</h3>
+              </div>
+            </div>
+          </div>
+          <div className="tech_team img2">
+            <div className="row">
+              <div style={{ width: "100%" }}>
+                <div className="appDevelopers_wrapper">{appDevelopers}</div>
+              </div>
+              <div className="team_title">
+                <h3 className="text-left text-white">Flutter Developers</h3>
+              </div>
+            </div>
+          </div>
+          <div className="tech_team img3">
+            <div className="row">
+              <div style={{ width: "100%" }}>
+                <div className="appDevelopers_wrapper">{appDevelopers}</div>
+              </div>
+              <div className="team_title">
+                <h3 className="text-center">Web Developers</h3>
+              </div>
+            </div>
+          </div>
+          <div className="tech_team img4">
+            <div className="row">
+              <div style={{ width: "100%" }}>
+                <div className="appDevelopers_wrapper">{appDevelopers}</div>
+              </div>
+              <div className="team_title">
+                <h3 className="text-left text-white">
+                  Machine Learning Enthusiasts
+                </h3>
+              </div>
+            </div>
+          </div>
+          <div className="tech_team img1">
+            <div className="row">
+              <div style={{ width: "100%" }}>
+                <div className="appDevelopers_wrapper ">{appDevelopers}</div>
+              </div>
+              <div className="team_title">
+                <h3 className="text-left">Cloud Engineers</h3>
+              </div>
+            </div>
+          </div>
+          <div className="tech_team img2">
+            <div className="row">
+              <div style={{ width: "100%" }}>
+                <div className="appDevelopers_wrapper ">{appDevelopers}</div>
+              </div>
+              <div className="team_title">
+                <h3 className="text-left text-white">AR / VR</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <br />
+        <div className="container">
+          <h3 className="text-center"> Non - Tech Team </h3>
+          <div className="divider"></div>
+          <div className="card nonTechTeam">
+            <div className="container">
+              <br />
+              <h4 className="text-center">Graphic Designing</h4>
+
+              <div className="row ">{nonTech}</div>
+              <br />
+              <h4 className="text-center">Content Writing</h4>
+
+              <div className="row ">{nonTech}</div>
+              <br />
+              <h4 className="text-center">Marketing</h4>
+
+              <div className="row ">{nonTech}</div>
+              <br />
+            </div>
+          </div>
+        </div>
+        <br />
+        <br />
+      </div>
+      <br />
+    </div>
+  );
 }
