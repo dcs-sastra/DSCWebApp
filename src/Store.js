69f69c70c8140cce9bbad4b3dcda110ce2fca3b2
Arav1nd2 <aravindsrinivasan2000@gmail.com>
2019-12-26 11:10:16 +0530

    Image Temporary Fix

--- src/Store.js
+++ src/Store.js
@@ -1,282 +1,225 @@
 export default {
-  aboutDSC: `Developer Student Clubs is a community where everyone is welcome. We help students bridge the gap between theory and practice and grow their knowledge by providing a peer-to-peer learning environment, by conducting workshops, study jams and building solutions for local businesses.
+     aboutDSC: `Developer Student Clubs is a community where everyone is welcome. We help students bridge the gap between theory and practice and grow their knowledge by providing a peer-to-peer learning environment, by conducting workshops, study jams and building solutions for local businesses.
 Developer Student Clubs is a program supported by Google Developers.`,
-  clusters: [
-    {
-      name: "Android",
-      img: "android.svg",
-      backgroundColor: "green_bg",
-      link: "/team/#android"
-    },
-    {
-      name: "Flutter",
-      img: "flutterio-icon.svg",
-      backgroundColor: "blue_bg",
-      link: "/team/#flutter"
-    },
-    {
-      name: "Machine Learning",
-      img: "setting.svg",
-      backgroundColor: "blue_bg",
-      link: "/team/#ML"
-    },
-    {
-      name: "Web",
-      img: "coding.svg",
-      backgroundColor: "green_bg",
-      link: "/team/#web"
-    },
-    {
-      name: "Cloud",
-      img: "learning.svg",
-      backgroundColor: "blue_bg",
-      link: "/team/#cloud"
-    },
-    {
-      name: "VR / AR",
-      img: "vr-glasses.svg",
-      backgroundColor: "red_bg",
-      link: "/team/#ar_vr"
-    },
-    {
-      name: "Content Writing",
-      img: "article.svg",
-      backgroundColor: "yellow_bg",
-      link: "/team/#content_writing"
-    },
-    {
-      name: "Graphic Designing",
-      img: "tools.svg",
-      backgroundColor: "green_bg",
-      link: "/team/#graphics"
-    },
-    {
-      name: "Marketing",
-      img: "report.svg",
-      backgroundColor: "yellow_bg",
-      link: "/team/#marketing"
-    }
-  ],
-  teams: {
-    lead: {
-      name: "Kavin Raju S",
-      imgUrl: "Kavin Raju",
-      role: "DSC Lead, App Developer"
-    },
-    app: [
-      {
-        name: "Kathir Thaniyarasu",
-        imgUrl: "Kathir"
-      },
-      {
-        name: "TANVEER AHMAD H",
-        imgUrl: "Tanveer Ahmad"
-      },
-      {
-        name: "Arun Balaji TSR",
-        imgUrl: "arun"
-      },
-      {
-        name: "Sibi N",
-        imgUrl: "Sibi Nehru"
-      },
-      {
-        name: "Karthikeyan",
-        imgUrl: "Karthikeyan K"
-      },
-      {
-        name: "Ashwin Ram",
-        imgUrl: "Ashwin Ram"
-      },
-      {
-        name: "Seshan",
-        imgUrl: "male"
-      } 
-    ],
-    flutter : [
-      {
-        name: "Ashwin Ram",
-        imgUrl: "Ashwin Ram"
-      },
-      {
-        name: "Seshan",
-        imgUrl: "male"
-      }
-    ],
-    web: [
-      {
-        name: "Aravind Srinivasan",
-        imgUrl: "Aravind Srinivasan"
-      },
-      {
-        name: "G Sai Svas",
-        imgUrl: "male"
-      },
-      {
-        name: "Srivathsan S",
-        imgUrl: "Sri Vathsan"
-      },
-      {
-        name: "Vashanth Saravanan",
-        imgUrl: "Vashanth Saravanan"
-      },
-      {
-        name: "K Vamsi Krishna",
-        imgUrl: "karanam vamsi"
-      },
-      {
-        name: "T Manikanta Reddy",
-        imgUrl: "tmanikanta reddy"
-      },
-      {
-        name: "Jaikanth",
-        imgUrl: "male"
-      },
-      {
-        name: "PV Lakshmi Narayanan",
-        imgUrl: "male"
-      },
-      {
-        name: "Sharath Sriram",
-        imgUrl: "male"
-      },
-    ],
-    ml: [
-      {
-        name: "Anjineyulu TV",
-        imgUrl: "male"
-      },
-      {
-        name: "P Vijay Rama Raju",
-        imgUrl: "male"
-      },
-      {
-        name: "Sreenithi Balasubramanian",
-        imgUrl: "Sreenithi Balasubramanian"
-      },
-      {
-        name: "Vishal R",
-        imgUrl: "Vishal R"
-      },
-      {
-        name: "S Giteka",
-        imgUrl: "giteka senthilkumar"
-      },
-      {
-        name: "Sivaneshwar P",
-        imgUrl: "Sivaneshwar P"
-      },
-      {
-        name: "Gokul",
-        imgUrl: "male"
-      }
-    ],
-    cloud: [
-      {
-        name: "Sivaneshwar P",
-        imgUrl: "Sivaneshwar P"
-      },
-      {
-        name: "Rohith",
-        imgUrl: "male"
-      }
-    ],
+     clusters: [
+          {
+               name: "Android",
+               img: "android.svg",
+               backgroundColor: "green_bg",
+               link: "/team/#android"
+          },
+          {
+               name: "Flutter",
+               img: "flutterio-icon.svg",
+               backgroundColor: "blue_bg",
+               link: "/team/#flutter"
+          },
+          {
+               name: "Machine Learning",
+               img: "setting.svg",
+               backgroundColor: "blue_bg",
+               link: "/team/#ML"
+          },
+          {
+               name: "Web",
+               img: "coding.svg",
+               backgroundColor: "green_bg",
+               link: "/team/#web"
+          },
+          {
+               name: "Cloud",
+               img: "learning.svg",
+               backgroundColor: "blue_bg",
+               link: "/team/#cloud"
+          },
+          {
+               name: "VR / AR",
+               img: "vr-glasses.svg",
+               backgroundColor: "red_bg",
+               link: "/team/#ar_vr"
+          },
+          {
+               name: "Content Writing",
+               img: "article.svg",
+               backgroundColor: "yellow_bg",
+               link: "/team/#content_writing"
+          },
+          {
+               name: "Graphic Designing",
+               img: "tools.svg",
+               backgroundColor: "green_bg",
+               link: "/team/#graphics"
+          },
+          {
+               name: "Marketing",
+               img: "report.svg",
+               backgroundColor: "yellow_bg",
+               link: "/team/#marketing"
+          }
+     ],
+     teams: {
+          lead: {
+               name: "Kavin Raju S",
+               imgUrl: "Kavin Raju",
+               role: "DSC Lead, App Developer"
+          },
+          app: [
+               {
+                    name: "Kathir Thaniyarasu",
+                    imgUrl: "Kathir"
+               },
+               {
+                    name: "TANVEER AHMAD H",
+                    imgUrl: "Tanveer Ahmad"
+               },
+               {
+                    name: "Sibi N",
+                    imgUrl: "Sibi Nehru"
+               },
+               {
+                    name: "Karthikeyan",
+                    imgUrl: "Karthikeyan K"
+               }
+          ],
+          flutter: [
+               {
+                    name: "Ashwin Ram",
+                    imgUrl: "Ashwin Ram"
+               },
+               {
+                    name: "Seshan",
+                    imgUrl: "male"
+               }
+          ],
+          web: [
+               {
+                    name: "Aravind Srinivasan",
+                    imgUrl: "Aravind Srinivasan"
+               },
+               {
+                    name: "Srivathsan S",
+                    imgUrl: "Sri Vathsan"
+               },
+               {
+                    name: "Vashanth Saravanan",
+                    imgUrl: "Vashanth Saravanan"
+               },
+               {
+                    name: "Jaikanth",
+                    imgUrl: "male"
+               },
+               {
+                    name: "PV Lakshmi Narayanan",
+                    imgUrl: "male"
+               }
+          ],
+          ml: [
+               {
+                    name: "Anjineyulu TV",
+                    imgUrl: "male"
+               },
+               {
+                    name: "P Vijay Rama Raju",
+                    imgUrl: "male"
+               },
+               {
+                    name: "Sreenithi Balasubramanian",
+                    imgUrl: "Sreenithi Balasubramanian"
+               },
+               {
+                    name: "Vishal R",
+                    imgUrl: "Vishal R"
+               },
+               {
+                    name: "S Giteka",
+                    imgUrl: "giteka senthilkumar"
+               },
+               {
+                    name: "Sivaneshwar P",
+                    imgUrl: "Sivaneshwar P"
+               },
+               {
+                    name: "Gokul",
+                    imgUrl: "male"
+               }
+          ],
+          cloud: [
+               {
+                    name: "Sivaneshwar P",
+                    imgUrl: "Sivaneshwar P"
+               }
+          ],
 
-    arvr: [
-      {
-        name: "Sivaneshwar P",
-        imgUrl: "Sivaneshwar P"
-      },
-      {
-        name: "Rohith",
-        imgUrl: "male"
-      }
-    ],
-  
-      graphic: [
-        {
-          name: "Kapil",
-          imgUrl: "male"
-        }
-      ],
-      content: [
-        {
-          name: "Rogini S",
-          imgUrl: "Rogini"
-        },
-        {
-          name: "Akhil Bharadwaj",
-          imgUrl: "male"
-        },
-        {
-          name: "Subhashri",
-          imgUrl: "female"
-        }
-      ],
-      marketing: [
-        {
-          name: "Nishanth S",
-          imgUrl: "Nishanth Sekar"
-        },
-        {
-          name: "Umesh Kanna",
-          imgUrl: "male"
-        },
-        {
-          name: "Syed Irfan",
-          imgUrl: "male"
-        },
-        {
-          name: "Mukunt B Surya",
-          imgUrl: "male"
-        }
-      ],
-      eventcoverage: [
-        {
-          name: "Nishanth S",
-          imgUrl: "Nishanth Sekar"
-        },
-        {
-          name: "Umesh Kanna",
-          imgUrl: "male"
-        },
-        {
-          name: "Syed Irfan",
-          imgUrl: "male"
-        },
-        {
-          name: "Mukunt B Surya",
-          imgUrl: "male"
-        }
-      ]
-    
-  },
-  events: [
-    {
-      id: 0,
-      name: "GCP Study Jam - Career Readiness Program",
-      date: "9 August 2019",
-      description: "Wanna become a cloud platform engineer? Here is the first step to become one! Exclusively sponsered by Google",
-      main:
-        "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-      venue: "Tifac Core III Floor",
-      speakers: "Kavin Raju",
-      poster: "gcp_poster.jpeg",
-      register_link: "https://google.com"
-    },
-    {
-      id: 1,
-      name: "Machine Learning Workshop",
-      date: "31 March 2019",
-      description: "Ever wondered will we ever use the concepts studied in Maths? Join Us for the workshop.",
-      main:
-        "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-      venue: "VV Auditorium",
-      speakers: "Vishal, Anjeneyulu",
-      poster: "mlposter.jpeg",
-      register_link: "https://google.com"
-    }
-  ]
+          arvr: [
+               {
+                    name: "Rohith",
+                    imgUrl: "male"
+               }
+          ],
+
+          graphic: [
+               {
+                    name: "Kapil",
+                    imgUrl: "male"
+               }
+          ],
+          content: [
+               {
+                    name: "Rogini S",
+                    imgUrl: "Rogini"
+               },
+               {
+                    name: "Akhil Bharadwaj",
+                    imgUrl: "male"
+               }
+          ],
+          marketing: [
+               {
+                    name: "Mukunt B Surya",
+                    imgUrl: "male"
+               }
+          ],
+          eventcoverage: [
+               {
+                    name: "Nishanth S",
+                    imgUrl: "Nishanth Sekar"
+               },
+               {
+                    name: "Umesh Kanna",
+                    imgUrl: "male"
+               },
+               {
+                    name: "Syed Irfan",
+                    imgUrl: "male"
+               }
+          ]
+     },
+     events: [
+          {
+               id: 0,
+               name: "GCP Study Jam - Career Readiness Program",
+               date: "9 August 2019",
+               description:
+                    "Wanna become a cloud platform engineer? Here is the first step to become one! Exclusively sponsered by Google",
+               main:
+                    "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+               venue: "Tifac Core III Floor",
+               speakers: "Kavin Raju",
+               poster: "gcp_poster.jpeg",
+               register_link: "https://google.com"
+          },
+          {
+               id: 1,
+               name: "Machine Learning Workshop",
+               date: "31 March 2019",
+               description:
+                    "Ever wondered will we ever use the concepts studied in Maths? Join Us for the workshop.",
+               main:
+                    "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+               venue: "VV Auditorium",
+               speakers: "Vishal, Anjeneyulu",
+               poster: "mlposter.jpeg",
+               register_link: "https://google.com"
+          }
+     ]
 };
-
-
