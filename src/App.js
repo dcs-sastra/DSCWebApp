cdd52aeaebab851d1a9dcd7d205956f0f28fd5c7
Aravind Srinivasan <aravindsrinivasan2000@gmail.com>
2020-03-20 20:23:40 +0530

    FEAT : Teams Page Done

--- src/App.js
+++ src/App.js
@@ -1,161 +1,141 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import android from "./android.svg";
 import bg1 from "./Backgrounds/BG_1.png";
 import bg2 from "./Backgrounds/BG_2.png";
-
-function importAll(r) {
-  let images = {};
-  r.keys().map(item => {
-    images[item.replace("./", "")] = r(item);
-  });
-  return images;
-}
-
-const clusters = importAll(
-  require.context("./clusters", false, /\.(png|jpe?g|svg)$/)
-);
+import Cluster from "./Cluster";
+import arrow from "./Logos/down-arrow.svg";
+import loader from "./loader.gif";
 
 function getRandomNumber() {
   return parseInt(1 + Math.random() * 99);
 }
 
 const App = () => {
-  const thumbs = [];
-  for (let i = 0; i <= 6; i++) {
-    let num = getRandomNumber();
-    thumbs.push(
-      <img src={`https://randomuser.me/api/portraits/men/${num}.jpg`} />
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch("https://dscsastraapi.herokuapp.com/MembersbyCluster")
+      .then(res => res.json())
+      .then(jsonData => setData(jsonData[0]));
+  }, [setData]);
+  console.log(data);
+  if (data == null) {
+    return (
+      <div className="loader">
+        <img src={loader} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <img src={bg1} className="bg1" />
+        <img src={bg2} className="bg2" />
+        <nav>
+          <h3>DSC SASTRA</h3>
+        </nav>
+        <main className="jumbotron">
+          <div className="quote-wrapper">
+            <h1>Alone we do so little. Together can change the world</h1>
+            <h2>We are team DSC SASTRA </h2>
+          </div>
+          <a href="#main-team" className="scroller">
+            <img src={arrow} />
+          </a>
+        </main>
+        <main id="main-team">
+          <section className="lead">
+            <div>
+              <img src={data.lead.imgUrl} style={{ width: "150px" }} />
+              <h3>{data.lead.name}</h3>
+              <p>{data.lead.role}</p>
+            </div>
+          </section>
+          <section className="others">
+            <Cluster
+              name="android"
+              img="android"
+              color="green"
+              right={true}
+              id={0}
+              data={data.app}
+            />
+            <Cluster
+              name="web"
+              img="coding"
+              right={false}
+              color="blue"
+              id={1}
+              data={data.web}
+            />
+            <Cluster
+              name="machine learning"
+              img="learning"
+              color="yellow"
+              right={true}
+              id={2}
+              data={data.ml}
+            />
+
+            <Cluster
+              name="AR / VR"
+              img="vr-glasses"
+              right={false}
+              color="red"
+              id={3}
+              data={data.arvr}
+            />
+            <Cluster
+              name="Flutter"
+              img="flutterio-icon"
+              color="blue"
+              right={true}
+              id={4}
+              data={data.flutter}
+            />
+            <Cluster
+              name="Cloud"
+              img="setting"
+              right={false}
+              color="yellow"
+              id={5}
+              data={data.cloud}
+            />
+            <Cluster
+              name="Graphic Designing"
+              img="tools"
+              color="green"
+              right={true}
+              id={6}
+              data={data.graphic}
+            />
+            <Cluster
+              name="Content Writing"
+              img="report"
+              right={false}
+              color="blue"
+              id={7}
+              data={data.content}
+            />
+            <Cluster
+              name="Marketing"
+              img="setting"
+              color="red"
+              right={true}
+              id={8}
+              data={data.marketing}
+            />
+            <Cluster
+              name="Event Coverage"
+              img="android"
+              right={false}
+              color="yellow"
+              id={9}
+              data={data.eventcoverage}
+            />
+          </section>
+        </main>
+      </div>
     );
   }
-
-  const android_members = "";
-
-  const [clusterState, setClusterState] = useState([
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false
-  ]);
-
-  const handleClusterClick = id => {
-    console.log(clusterState);
-    let newClusterState = clusterState.slice();
-    newClusterState[id] = !newClusterState[id];
-    setClusterState(newClusterState);
-  };
-  return (
-    <div className="App">
-      <img src={bg1} className="bg1" />
-      <img src={bg2} className="bg2" />
-      <nav>
-        <h3>DSC SASTRA</h3>
-      </nav>
-      <main>
-        <section className="lead">
-          <div>
-            <img
-              src={`https://randomuser.me/api/portraits/men/${getRandomNumber()}.jpg`}
-              style={{ width: "100px" }}
-            />
-            <h3>Kavin Raju S</h3>
-            <p>Lead</p>
-          </div>
-        </section>
-        <section className="others">
-          <div className="cluster" onClick={() => handleClusterClick(0)}>
-            <section className="green">
-              <h3>Android</h3>
-              <div className="thumbs">{thumbs}</div>
-              {clusterState[0] && (
-                <div className="drawer">{android_members}</div>
-              )}
-            </section>
-            <h3>Android</h3>
-            <img src={clusters["android.svg"]} />
-          </div>
-          <div className="cluster">
-            <img src={clusters["coding.svg"]} />
-            <h3>Web</h3>
-            <section className="blue">
-              <h3>Web</h3>
-              <div className="thumbs">{thumbs}</div>
-            </section>
-          </div>
-          <div className="cluster">
-            <section className="yellow">
-              <h3>Machine Learning</h3>
-              <div className="thumbs">{thumbs}</div>
-            </section>
-            <h3>Machine Learning</h3>
-            <img src={clusters["learning.svg"]} />
-          </div>
-          <div className="cluster">
-            <img src={clusters["vr-glasses.svg"]} />
-            <h3>AR / VR</h3>
-            <section className="red">
-              <h3>AR / VR</h3>
-              <div className="thumbs">{thumbs}</div>
-            </section>
-          </div>
-          <div className="cluster">
-            <section className="blue">
-              <h3>Flutter</h3>
-              <div className="thumbs">{thumbs}</div>
-            </section>
-            <h3>Flutter</h3>
-            <img src={clusters["flutterio-icon.svg"]} />
-          </div>
-          <div className="cluster">
-            <img src={clusters["setting.svg"]} />
-            <h3>Cloud</h3>
-            <section className="yellow">
-              <h3>Cloud</h3>
-              <div className="thumbs">{thumbs}</div>
-            </section>
-          </div>
-          <div className="cluster">
-            <section className="green">
-              <h3>Graphic Designing</h3>
-              <div className="thumbs">{thumbs}</div>
-            </section>
-            <h3>Graphic Designing</h3>
-            <img src={clusters["tools.svg"]} />
-          </div>
-          <div className="cluster">
-            <img src={clusters["report.svg"]} />
-            <h3>Content Writing</h3>
-            <section className="blue">
-              <h3>Content Writing</h3>
-              <div className="thumbs">{thumbs}</div>
-            </section>
-          </div>
-          <div className="cluster">
-            <section className="red">
-              <h3>Marketing</h3>
-              <div className="thumbs">{thumbs}</div>
-            </section>
-            <h3>Marketing</h3>
-            <img src={clusters["setting.svg"]} />
-          </div>
-          <div className="cluster">
-            <img src={android} />
-            <h3>Event Coverage</h3>
-            <section className="yellow">
-              <h3>Event Coverage</h3>
-              <div className="thumbs">{thumbs}</div>
-            </section>
-          </div>
-        </section>
-      </main>
-    </div>
-  );
 };
 
 export default App;
---
+++ src/Cluster.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import "./cluster.css";
+import git from "./Logos/github.svg";
+import linkedin from "./Logos/linkedin.svg";
+
+function importAll(r) {
+  let images = {};
+  r.keys().map(item => {
+    images[item.replace("./", "")] = r(item);
+  });
+  return images;
+}
+
+const clusters = importAll(
+  require.context("./clusters", false, /\.(png|jpe?g|svg)$/)
+);
+
+function getRandomNumber() {
+  return parseInt(1 + Math.random() * 99);
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const Cluster = props => {
+  const [clusterState, setClusterState] = useState(false);
+  const handleClusterClick = () => {
+    setClusterState(!clusterState);
+  };
+  const thumbs = props.data.map((person, id) => (
+    <img
+      src={
+        person.imgUrl === ""
+          ? `https://randomuser.me/api/portraits/men/${getRandomNumber()}.jpg`
+          : person.imgUrl
+      }
+    />
+  ));
+  const width = window.screen.width;
+  const members = props.data.map((person, id) => (
+    <div key={id} className="member">
+      <img
+        src={
+          person.imgUrl === ""
+            ? `https://randomuser.me/api/portraits/men/${getRandomNumber()}.jpg`
+            : person.imgUrl
+        }
+      />
+      <p>{person.name}</p>
+      <div className="social">
+        <a href={person.github} target="_blank">
+          <img src={git} />
+        </a>
+        <a href={person.linkedin} target="_blank">
+          <img src={linkedin} />
+        </a>
+      </div>
+    </div>
+  ));
+
+  const newStyles = {
+    flexFlow: clusterState ? "column" : ""
+  };
+  const sectionStyle = {
+    width: clusterState ? "40%" : "",
+    height: clusterState ? "8vh" : ""
+  };
+  let title = "";
+  if (width < 600) {
+    title = <h3>{capitalizeFirstLetter(props.name)}</h3>;
+  } else if (clusterState == true) {
+    title = <h3>{capitalizeFirstLetter(props.name)}</h3>;
+  } else {
+    title = "";
+  }
+  if (props.right) {
+    return (
+      <div
+        className="cluster"
+        onClick={() => handleClusterClick(0)}
+        style={newStyles}
+      >
+        <section className={props.color} style={sectionStyle}>
+          {title}
+          <div className="thumbs">{!clusterState && thumbs}</div>
+        </section>
+        {!clusterState && <h3>{capitalizeFirstLetter(props.name)}</h3>}
+        {!clusterState && <img src={clusters[`${props.img}.svg`]} />}
+        {clusterState && <div className="drawer">{members}</div>}
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className="cluster"
+        onClick={() => handleClusterClick(0)}
+        style={newStyles}
+      >
+        {!clusterState && <img src={clusters[`${props.img}.svg`]} />}
+        {!clusterState && <h3>{capitalizeFirstLetter(props.name)}</h3>}
+        <section className={props.color} style={sectionStyle}>
+          {title}
+          <div className="thumbs">{!clusterState && thumbs}</div>
+        </section>
+        {clusterState && <div className="drawer">{members}</div>}
+      </div>
+    );
+  }
+};
+
+export default Cluster;
