68a78d8c4ae5c8b18749fd4ac1f4358bb9de2d27
ayushsingh12march <ayushsingh12march@gmail.com>
2020-06-10 16:26:46 +0530

    styled buttons

--- src/Pages/About/Buttons.js
+++ src/Pages/About/Buttons.js
@@ -4,8 +4,8 @@ import med from "./assets/med.png";
 import playStore from "./assets/playStore.png"
 import youTube from "./assets/youTube.png"
 import insta from "./assets/insta.png"
-import linkedin from "../Footer/linkedin.svg"
-import twitter from "../Footer/twitter.svg"
+import linkedin from "./assets/linkedIn.png"
+import twitter from "./assets/twitter.png"
 
 const Buttons = ({type}) => {
     if(type===1){
@@ -24,11 +24,10 @@ const Buttons = ({type}) => {
                 <img src={insta} alt="instagram" width="40px" />
             </button>    
             <button 
-                className="twitter"
                 onClick={() => {
                     window.open("https://twitter.com/dsc_sastra");
                   }}>
-                <img  src={twitter} alt="twitter" width="25px" />
+                <img  src={twitter} alt="twitter" width="40px" />
             </button>
             <button  onClick={() => {
                 window.open("#");
@@ -36,11 +35,11 @@ const Buttons = ({type}) => {
                 <img src={fb} alt="fb" width="40px" />
             </button>
             <button 
-                className="linkedIn" 
+                
                 onClick={() => {
                 window.open("https://www.linkedin.com/company/dsc-sastra")
               }}>
-                <img id="linkedIn" src={linkedin} alt="linkedIn" width="25px" />
+                <img  src={linkedin} alt="linkedIn" width="40px" />
             </button>
             <button 
                 onClick={() => {
