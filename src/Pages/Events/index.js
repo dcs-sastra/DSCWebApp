c565862bcaace3514b9a63906d773e08fe3b2c89
Vashanth <2000.vashanth@gmail.com>
2020-07-02 12:59:48 +0530

    prev,next button in events opacity

--- src/Pages/Events/index.js
+++ src/Pages/Events/index.js
@@ -6,6 +6,7 @@ const eventsPerPage = 5;
 const EventMain = () => {
   const [events, setEvents] = useState([]);
   const [pageNo, setPageNo] = useState(1);
+  const [direction, setDirection] = useState("left")
   useEffect(() => {
     const loadData = async () => {
       let data = await fetch(`https://dscsastraapi.herokuapp.com/events`);
@@ -20,12 +21,21 @@ const EventMain = () => {
   const eventsRendered = events.slice(startIndex, endIndex);
   console.log(eventsRendered);
   const next = () => {
-    if (pageNo * eventsPerPage <= events.length) {
+    if (pageNo * eventsPerPage < events.length) {
+      console.log(pageNo * eventsPerPage,events.length)
+      if(pageNo * eventsPerPage >= events.length-eventsPerPage)
+      setDirection("right")
+      else
+      setDirection("center")
       setPageNo(pageNo + 1);
     }
   };
   const prev = () => {
     if (pageNo != 1) {
+      if(pageNo==2)
+      setDirection("left")
+      else
+      setDirection("center")
       setPageNo(pageNo - 1);
     }
   };
@@ -37,7 +47,7 @@ const EventMain = () => {
     );
   } else {
     return (
-      <Events events={eventsRendered} page={pageNo} next={next} prev={prev} />
+      <Events events={eventsRendered} page={pageNo} next={next} prev={prev} direction={direction}/>
       // <Events events={events} />
     );
   }
