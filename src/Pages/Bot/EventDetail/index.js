93a709fb4ab5a4764fb9bf04bd63580ade47cb13
Jaikanth J <jaikanthjay46@gmail.com>
2020-08-23 14:54:57 +0530

    add: Rich Events for Bots

---
+++ src/Pages/Bot/EventDetail/index.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+
+const EventBotDetail = () => {
+
+    const [event, setEvent] = useState({});
+    const params = useParams(); 
+    useEffect(() => {
+        const loadData = () => {
+        fetch(`https://dscsastraapi.herokuapp.com/events/${params.id}`)
+            .then( res => res.json() )
+            .then( data => setEvent(data))
+        };
+        loadData();
+    }, []);
+
+    const getJSON = () => {
+        return JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Event",
+            "name": event.name,
+            "startDate": event.date,
+            "eventStatus": "https://schema.org/EventScheduled",
+            "eventAttendanceMode": "https://schema.org/OnlineEventAttendanceMode",
+            "location": {
+              "@type": "VirtualLocation",
+              "url": event.register_link
+              },
+            "image": [
+              event.poster
+             ],
+            "description": event.description,
+            "performer": {
+              "@type": "PerformingGroup",
+              "name": event.speakers
+            },
+            "organizer": {
+                "@type": "Organization",
+                "name": "DSC SASTRA",
+                "url": "https://dscsastra.com"
+            }
+          })
+    }
+    
+
+    return (
+        <script type="application/ld+json">
+        {getJSON()}
+        </script>
+    );
+  
+};
+
+export default EventBotDetail;
